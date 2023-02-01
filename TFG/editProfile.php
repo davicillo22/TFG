b7628ef0a119a35de5e5a12d672a0f2090eb84f7
David <ddomin04@ucm.es>
2023-02-01 20:38:16 +0100

    Add files via upload

--- TFG/editProfile.php
+++ TFG/editProfile.php
@@ -3,7 +3,7 @@
 require_once __DIR__.'/includes/config.php';
 require_once __DIR__.'/includes/usuarios.php';
 
-$tituloPagina = 'Mi perfil';
+$tituloPagina = 'Mi perfil 222';
 
 $contenidoPrincipal=datosUsuario($_SESSION["email"]);
 $contenidoPrincipal.= <<<EOS
