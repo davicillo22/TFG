ab08010ca8f38aff6fe3a7c727ea7f089a3cabb0
AlexVonex <alemilla@ucm.es>
2023-02-17 20:37:18 +0100

    mediomod

--- TFG/tablaPacientes.php
+++ TFG/tablaPacientes.php
@@ -13,8 +13,183 @@ else {
     $nhisFound = isset($_GET["nhisFound"]) ? $_GET["nhisFound"] : true;
 }
 
-$contenidoPrincipal= <<<'EOS'
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
+
+if (isset($_POST['submit'])) {
+    $columna = $_POST['columna'];
+    $operacion = $_POST['operacion'];
+    $valor = $_POST['valor'];
+
+    if ($operacion == 'contains') {
+        $condicion = "LOWER($columna) LIKE LOWER('%$valor%')";
+    } else {
+        $condicion = "$columna $operacion '$valor'";
+    }
+
+    if (!isset($_SESSION['filtros'])) {
+        $_SESSION['filtros'] = array();
+    }
+
+    array_push($_SESSION['filtros'], $condicion);
+}
+
+if (isset($_POST['union'])) {
+    $union = $_POST['union'];
+
+    if (!isset($_SESSION['condiciones'])) {
+        $_SESSION['condiciones'] = array();
+    }
+
+    $condicion = implode(" $union ", $_SESSION['filtros']);
+    array_push($_SESSION['condiciones'], $condicion);
+    unset($_SESSION['filtros']);
+}
+
+if (isset($_SESSION['condiciones'])) {
+    $condicion = implode(" AND ", $_SESSION['condiciones']);
+} else {
+    $condicion = '1=1';
+}
+//conectamos con la bbdd
+$conn = mysqli_connect("localhost", "root", "", "bbdd");
+
+// construir consulta SQL
+$sql = "SELECT * FROM patients WHERE $condicion";
+
+// ejecutar consulta
+$result = $conn->query($sql);
+
+// manejar resultados - FALTARÍA QUE SE APLIQUEN LOS FILTROS EN LA TABLA
+
+// Close the connection
+
+//check nhis
+mysqli_close($conn);
+$textoBusqueda = "Introduzca el NHIS a buscar";
+$colorSearch="gray";
+if (!$nhisFound){
+    $textoBusqueda= "NHIS no encontrado";
+    $colorSearch="red";
+}
+
+$contenidoPrincipal= <<<EOS
+<link rel="stylesheet" href="css/tableStyle.css">
+<style>
+		/* Estilo para el placeholder del input text */
+		::-webkit-input-placeholder { /* Para navegadores basados en Webkit */
+			color: $colorSearch;
+		}
+		:-moz-placeholder { /* Para navegadores basados en Mozilla Firefox */
+			color: $colorSearch;
+		}
+		::-moz-placeholder { /* Para navegadores basados en Mozilla Firefox */
+			color: $colorSearch;
+		}
+		:-ms-input-placeholder { /* Para navegadores basados en Microsoft Edge */
+			color:  $colorSearch;
+		}
+	</style>
+	<div style="width: auto; height: 50px; margin: auto; margin-top: 40px; margin-bottom: -75px;""> 
+	
+    <form class="search-container"  action="searchPatient.php" method="post">
+  
+        <input  type="text" name="nhis" id="idInput" placeholder='$textoBusqueda'>
+        <button id="searchBtn">Search</button>
+    </form>
+
+</div> 
+<div style="width: 1500px; height: 466px; overflow: auto; margin: 0 auto; margin-top: 40px; margin-bottom: -480px;">
+  <a class='btn btn-success btn-lg' href='addPatient.php'>Add Patient</a>
+  </div>
+
+EOS;
+
+
+$addPatientButton = "<div style='width: 1500px; height: 60px; overflow: auto; margin: 0 auto; margin-top: 15px; '>
+    <a class='btn btn-success btn-lg' href='addPatient.php'>Add Patient</a>
+</div>";
+
+$tabla = "";
+
+
+// Create variables for sorting
+$sort_col = isset($_GET['sort_col']) ? $_GET['sort_col'] : '';
+$sort_order = isset($_GET['sort_order']) ? $_GET['sort_order'] : 'asc';
+
+// Connect to the database
+$conn = mysqli_connect("localhost", "root", "", "bbdd");
+
+// Check the connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// Write the SQL query
+$sql = "SELECT * FROM patients";
+if (!empty($sort_col)) {
+    $sql .= " ORDER BY ".$sort_col." ".$sort_order;
+}
+
+// Execute the query
+$result = mysqli_query($conn, $sql);
+
+// Check if the query was successful
+if (mysqli_num_rows($result) > 0) {
+    $tabla .= <<<EOS
+        <div style="width: 1500px; height: 466px; overflow: auto; margin: 0 auto; margin-top: 80px; outline: 3px solid black;">
+        EOS;
+  $tabla .= "<table>";
+  $tabla .= "<tr>";
+
+    // Get the column names
+    $column_names = mysqli_fetch_fields($result);
+    foreach ($column_names as $column_name) {
+        // Check if this is the current sort column
+        if ($column_name->name == $sort_col) {
+            // Switch the sort order
+            $sort_order = ($sort_order == 'asc') ? 'desc' : 'asc';
+            // Add sorting link with arrow icon
+            $tabla .= "<th ><a href='?sort_col=".$column_name->name."&sort_order=".$sort_order."'>".$column_name->name." <i class='fa fa-arrow-".($sort_order == 'asc' ? 'up' : 'down')."'></i></a></th>";
+        } else {
+            // Add sorting link without arrow icon
+            $tabla .= "<th><a href='?sort_col=".$column_name->name."&sort_order=asc'>".$column_name->name."</a></th>";
+        }
+    }
+
+    $tabla .= "</tr>";
+
+  // Loop through the results and print each row
+  while ($row = mysqli_fetch_assoc($result)) {
+    $tabla .= "<tr>";
+    $i = 0;
+    foreach ($column_names as $column_name) {
+        if ($i == 0){
+            $tabla .= "<td><a style='color: blue;' href='searchPatient.php?id=" . $row[$column_name->name] . "'>" . $row[$column_name->name] . "</a></td>";
+        }
+        else{
+            $tabla .= "<td>" . $row[$column_name->name] . "</td>";
+        }
+        $i++;
+    }
+    $tabla .= "</tr>";
+  }
+
+  $tabla .= "</table>";
+} else {
+  $tabla .= "<div>0 results</div>";
+}
+
+// Close the connection
+mysqli_close($conn);
+
+
+$contenidoPrincipal .= $tabla;
+$contenidoPrincipal .= "</div>";
+$contenidoPrincipal.= <<<EOS
+<link rel="stylesheet" href="css/filterStyle.css">
+<div >
+<h3 style="margin-left: 40%; margin-top: 2%;" >Filtrar Pacientes</h3>
+<form  method="post" action="procesarFiltros.php">
     <select name="columna">
         <option value="fechacir">Fechacir</option>
         <option value="edad">Edad</option>
@@ -81,164 +256,22 @@ $contenidoPrincipal= <<<'EOS'
         <option value=">">Mayor que</option>
         <option value="contains">Contiene</option>
     </select>
-    <input type="text" name="valor">
-    <input type="submit" name="submit" value="Filtrar">
+    <input style="height: 2%;" type="text"  name="valor">
+    <input  type="submit" name="submit" value="Filtrar">
 </form>
-<br>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
+<form  method="post" action="procesarFiltros.php">
     <select name="union">
         <option value="and">Y</option>
         <option value="or">Ó</option>
     </select>
     <input type="submit" name="submit" value="Agregar filtro">
 </form>
-<br>
-EOS;
-
-if (isset($_POST['submit'])) {
-    $columna = $_POST['columna'];
-    $operacion = $_POST['operacion'];
-    $valor = $_POST['valor'];
-
-    if ($operacion == 'contains') {
-        $condicion = "LOWER($columna) LIKE LOWER('%$valor%')";
-    } else {
-        $condicion = "$columna $operacion '$valor'";
-    }
-
-    if (!isset($_SESSION['filtros'])) {
-        $_SESSION['filtros'] = array();
-    }
-
-    array_push($_SESSION['filtros'], $condicion);
-}
-
-if (isset($_POST['union'])) {
-    $union = $_POST['union'];
-
-    if (!isset($_SESSION['condiciones'])) {
-        $_SESSION['condiciones'] = array();
-    }
-
-    $condicion = implode(" $union ", $_SESSION['filtros']);
-    array_push($_SESSION['condiciones'], $condicion);
-    unset($_SESSION['filtros']);
-}
-
-if (isset($_SESSION['condiciones'])) {
-    $condicion = implode(" AND ", $_SESSION['condiciones']);
-} else {
-    $condicion = '1=1';
-}
-//conectamos con la bbdd
-$conn = mysqli_connect("localhost", "root", "", "bbdd");
-
-// construir consulta SQL
-$sql = "SELECT * FROM patients WHERE $condicion";
-
-// ejecutar consulta
-$result = $conn->query($sql);
-
-// manejar resultados - FALTARÍA QUE SE APLIQUEN LOS FILTROS EN LA TABLA
-
-// Close the connection
-mysqli_close($conn);
-
-$contenidoPrincipal= <<<EOS
-<link rel="stylesheet" href="css/tableStyle.css">
-<div style="width: 1500px; height: 50px; margin: 0 auto; margin-top: 50px;">
-	  
-    <form class="search-container" action="searchPatient.php" method="post">
-        <input type="text" name="nhis" id="idInput" placeholder="Introduzca el NHIS a buscar">
-        <button id="searchBtn">Search</button>
-    </form>
 
 </div>
-<div style="width: 1500px; height: 600px; overflow: auto; margin: 0 auto; margin-top: 30px; outline: 2px solid black;">
+</body></html>
 EOS;
-$addPatientButton = "<div style='width: 1500px; height: 60px; overflow: auto; margin: 0 auto; margin-top: 15px; '>
-    <a class='btn btn-success btn-lg' href='addPatient.php'>Add Patient</a>
-</div>";
 
-$tabla = "";
-
-
-// Create variables for sorting
-$sort_col = isset($_GET['sort_col']) ? $_GET['sort_col'] : '';
-$sort_order = isset($_GET['sort_order']) ? $_GET['sort_order'] : 'asc';
-
-// Connect to the database
-$conn = mysqli_connect("localhost", "root", "", "bbdd");
-
-// Check the connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-// Write the SQL query
-$sql = "SELECT * FROM patients";
-if (!empty($sort_col)) {
-    $sql .= " ORDER BY ".$sort_col." ".$sort_order;
-}
-
-// Execute the query
-$result = mysqli_query($conn, $sql);
-
-// Check if the query was successful
-if (mysqli_num_rows($result) > 0) {
-  $tabla .= "<table>";
-  $tabla .= "<tr>";
-
-    // Get the column names
-    $column_names = mysqli_fetch_fields($result);
-    foreach ($column_names as $column_name) {
-        // Check if this is the current sort column
-        if ($column_name->name == $sort_col) {
-            // Switch the sort order
-            $sort_order = ($sort_order == 'asc') ? 'desc' : 'asc';
-            // Add sorting link with arrow icon
-            $tabla .= "<th><a href='?sort_col=".$column_name->name."&sort_order=".$sort_order."'>".$column_name->name." <i class='fa fa-arrow-".($sort_order == 'asc' ? 'up' : 'down')."'></i></a></th>";
-        } else {
-            // Add sorting link without arrow icon
-            $tabla .= "<th><a href='?sort_col=".$column_name->name."&sort_order=asc'>".$column_name->name."</a></th>";
-        }
-    }
-
-    $tabla .= "</tr>";
-
-  // Loop through the results and print each row
-  while ($row = mysqli_fetch_assoc($result)) {
-    $tabla .= "<tr>";
-    $i = 0;
-    foreach ($column_names as $column_name) {
-        if ($i == 0){
-            $tabla .= "<td><a style='color: blue;' href='searchPatient.php?id=" . $row[$column_name->name] . "'>" . $row[$column_name->name] . "</a></td>";
-        }
-        else{
-            $tabla .= "<td>" . $row[$column_name->name] . "</td>";
-        }
-        $i++;
-    }
-    $tabla .= "</tr>";
-  }
-
-  $tabla .= "</table>";
-} else {
-  $tabla .= "<div>0 results</div>";
-}
-
-// Close the connection
-mysqli_close($conn);
-
-if (!$nhisFound){
-    $contenidoPrincipal.= "<h3 style='color: red;'>NHIS no encontrado.</h3><br>";
-
-    $contenidoPrincipal.= "</body></html>";
-}
-
-$contenidoPrincipal .= $tabla;
-$contenidoPrincipal .= "</div>";
-$contenidoPrincipal .= $addPatientButton;
 
 
 require __DIR__.'/includes/layout.php';
