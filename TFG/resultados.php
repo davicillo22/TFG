22badf17cc906bd75391baf5c67a140a95569f69
David <ddomin04@ucm.es>
2023-04-20 20:48:47 +0200

    results a probar

--- TFG/resultados.php
+++ TFG/resultados.php
@@ -8,6 +8,8 @@ $tituloPagina = 'Resultados';
 
 $contenidoPrincipal="";
 
+
+require __DIR__.'/includes/layout.php';
 $conn = mysqli_connect("localhost", "root", "", "bbdd");
 
 // Check the connection
@@ -15,79 +17,107 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$algoritmo = $_POST['algoritmo'];
-$variable = $_POST['variable'];
+$algoritmo = 'regresion'; //$_POST['algoritmo'];
+$variable = 'extracap'; //$_POST['variable'];
 
+//$algoritmo = isset($_POST['algoritmo']) ? $_POST['algoritmo'] : '';
+//$variable = isset($_POST['variable']) ? $_POST['variable'] : '';
+?>
+
+    <div class="center-div">
+        <h2 style="color: black">Métricas para observar la variable: <?php echo $variable; ?>, con el algoritmo: <?php echo $algoritmo; ?></h2>
+    </div>
+
+<?php
 if($algoritmo == 'cox'){
     if($variable == 'rbqPre'){
+
         $rbq_5_years_pre = $_POST['rbq_5_years_pre'];
-        echo "RBQ a 5 años (preoperatorio): " . $rbq_5_years_pre . "<br>";
+        echo"<h3>RBQ a 5 años (preoperatorio): " . $rbq_5_years_pre . "</h3>";
         $rbq_10_years_pre = $_POST['rbq_10_years_pre'];
-        echo "RBQ a 10 años (preoperatorio): " . $rbq_10_years_pre . "<br>";
-        ?>
-        <img src="images/rbqpre.jpg" alt="Metricas para RBQ preoperatorio" width="300" height="200">
-        <?php
+        echo"<h3>RBQ a 10 años (preoperatorio): " . $rbq_10_years_pre . "</h3>";
+        echo"<h3>Concordance = 0.77 </h3>";
+        echo"<h3>Partial AIC = 172.69</h3>";
+        echo"<h3>log-likelihood ratio test = 9.83 on 30 df</h3>";
     }else if($variable == 'rbqPost'){
         $rbq_5_years_post = $_POST['rbq_5_years_post'];
-        echo "RBQ a 5 años (postoperatorio): " . $rbq_5_years_post . "<br>";
+        echo "</h3>RBQ a 5 años (postoperatorio): " . $rbq_5_years_post . "</h3>";
         $rbq_10_years_post = $_POST['rbq_10_years_post'];
-        echo "RBQ a 10 años (postoperatorio): " . $rbq_10_years_post . "<br>";
-        ?>
-        <img src="images/rbqpost.jpg" alt="Metricas para RBQ postoperatorio" width="300" height="200">
-        <?php
+        echo "</h3>RBQ a 10 años (postoperatorio): " . $rbq_10_years_post . "</h3>";
+        echo"</h3>Concordance = 0.93 </h3>";
+        echo"</h3>Partial AIC = 193.67</h3>";
+        echo"</h3>log-likelihood ratio test = 30.85 on 51 df </h3>";
     }
 }else{
     if ($variable == 'extracap') {
         if ($algoritmo == 'regresion') {
             $lr_probability = $_POST['lr_probability'];
-            echo "Regresion logística EXTRACAP: " . $lr_probability . "<br>";
-        }else {
+            echo "<h3>Regresion logística EXTRACAP: " . $lr_probability . "<br>";
+            echo "<h3>F1: 0,79</h3>";
+            echo "<h3>Recall: 0,77</h3>";
+            echo "<h3>Precision: 0,86</h3>";
+            echo "<h3>Accuracy: 0,92</h3>";
+        } else {
             $rf_probability = $_POST['rf_probability'];
-            echo "Arboles aleatorios EXTRACAP: " . $rf_probability . "<br>";
+            echo "<h3>Árboles aleatorios EXTRACAP: " . $rf_probability . "</h3>";
+            echo "<h3>F1: 0,90</h3>";
+            echo "<h3>Recall: 0,88</h3>";
+            echo "<h3>Precision: 0,93 </h3>";
+            echo "<h3>Accuracy: 0,96 <br>";
         }
-        ?>
-        <img src="images/extracap.jpg" alt="Metricas para EXTRACAP" width="300" height="200">
-        <?php
     }else if ($variable == 'margen') {
         if ($algoritmo == 'regresion') {
             $lr_probability = $_POST['lr_probability'];
-            echo "Regresion logística MARGEN: " . $lr_probability . "<br>";
+            echo "<h3>Regresion logística MARGEN: " . $lr_probability . "</h3>";
+            echo "<h3>F1: 0,81  </h3>";
+            echo "<h3>Recall: 0,76  </h3>";
+            echo "<h3>Precision: 0,85 </h3>";
+            echo "<h3>Accuracy: 0,92 </h3>";
 
         }else {
             $rf_probability = $_POST['rf_probability'];
-            echo "Arboles aleatorios MARGEN: " . $rf_probability . "<br>";
+            echo "<h3>Árboles aleatorios MARGEN: " . $rf_probability . "</h3>";
+            echo "<h3>F1: 0,64  </h3>";
+            echo "<h3>Recall: 0,58  </h3>";
+            echo "<h3>Precision: 0,76 </h3>";
+            echo "<h3>Accuracy: 0,84 </h3>";
         }
-        ?>
-        <img src="images/margen.jpg" alt="Metricas para MARGEN" width="300" height="200">
-        <?php
     } else if ($variable == 'tnm2') {
         if ($algoritmo == 'regresion') {
             $lr_probability = $_POST['lr_probability'];
-            echo "Regresion logística TNM2: " . $lr_probability . "<br>";
+            echo "<h3>Regresion logística TNM2: " . $lr_probability . "</h3>";
+            echo "<h3>F1: 0,95  </h3>";
+            echo "<h3>Recall: 0,95  </h3>";
+            echo "<h3>Precision: 0,95 </h3>";
+            echo "<h3>Accuracy: 0,95 </h3>";
 
         }else {
             $rf_probability = $_POST['rf_probability'];
-            echo "Arboles aleatorios TNM2: " . $rf_probability . "<br>";
+            echo "<h3>Árboles aleatorios TNM2: " . $rf_probability . "</h3>";
+            echo "<h3>F1: 0,97  </h3>";
+            echo "<h3>Recall: 0,97  </h3>";
+            echo "<h3>Precision: 0,97 </h3>";
+            echo "<h3>Accuracy: 0,97 </h3>";
         }
-        ?>
-        <img src="images/tnm2.jpg" alt="Metricas para TNM2" width="300" height="200">
-        <?php
     } else if ($variable == 'vvss') {
         if ($algoritmo == 'regresion') {
             $lr_probability = $_POST['lr_probability'];
-            echo "Regresión logística VVSS: " . $lr_probability . "<br>";
+            echo "<h3>Regresión logística VVSS: " . $lr_probability . "</h3>";
+            echo "<h3>F1: 0,68  </h3>";
+            echo "<h3>Recall: 0,72  </h3>";
+            echo "<h3>Precision: 0,67 </h3>";
+            echo "<h3>Accuracy: 0,92 </h3>";
 
         }else {
             $rf_probability = $_POST['rf_probability'];
-            echo "Arboles aleatorios VVSS: " . $rf_probability . "<br>";
+            echo "<h3>Árboles aleatorios VVSS: " . $rf_probability . "</h3>";
+            echo "<h3>F1: 0,50  </h3>";
+            echo "<h3>Recall: 0,43  </h3>";
+            echo "<h3>Precision: 0,64 </h3>";
+            echo "<h3>Accuracy: 0,91 </h3>";
         }
-        ?>
-        <img src="images/vvss.jpg" alt="Metricas para VVSS" width="300" height="200">
-        <?php
     }
 }
-
-
 
 
 
